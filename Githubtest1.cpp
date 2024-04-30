a9ecd174cb9212192227c1040931142ff86ebf37
Doug <64031739+MishaGromovPrlmn@users.noreply.github.com>
2024-04-30 14:14:19 -0400

    Add files via upload
    Upload test

---
+++ Githubtest1.cpp
@@ -0,0 +1,27 @@
+
+    // Online C++ compiler to run C++ program online
+    #include <iostream>
+    #include<bits/stdc++.h>
+    using namespace std;
+
+
+
+void dfs (){
+  
+
+  cout<<"Hello, Petr here"<<"\n";
+}
+
+
+
+
+
+
+    int main() {
+
+        solve();
+
+
+    return 0;
+
+    }
